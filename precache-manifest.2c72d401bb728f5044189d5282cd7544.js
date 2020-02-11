fd64fe77139b41f7fe6cc2d59c5b37b86eca9298
Travis <NguyenTrav@gmail.com>
2020-02-10 21:18:49 -0500

    Updates

---
+++ precache-manifest.2c72d401bb728f5044189d5282cd7544.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dae5b10d9b51d4bcc7a9d3e5af40f7d",
+    "url": "/dad-joke-app/index.html"
+  },
+  {
+    "revision": "0d59f203154ac1459e0a",
+    "url": "/dad-joke-app/static/css/main.50ef929e.chunk.css"
+  },
+  {
+    "revision": "5c2c2393ca2dbf10e4d9",
+    "url": "/dad-joke-app/static/js/2.f925769b.chunk.js"
+  },
+  {
+    "revision": "0d59f203154ac1459e0a",
+    "url": "/dad-joke-app/static/js/main.60fd9c33.chunk.js"
+  },
+  {
+    "revision": "438a9d72f2f4e01fbbf8",
+    "url": "/dad-joke-app/static/js/runtime-main.ffaf0718.js"
+  }
+]);
